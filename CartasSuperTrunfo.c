158bf97566bd891b05fe5acac258dd2d757a5ae0
Tiago Dourado da Silva <tdourado412@gmail.com>
2025-08-15 01:07:55 +0000

    Finalizar o desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     
     char estado;
     char código [20];
-    char omedaCidade [20];
+    char nomedaCidade [20];
     int população;
     float areakm²;
     float PIB;
@@ -13,11 +13,23 @@ int main() {
     printf("Qual e o estado: \n");
     scanf("%c", estado);
 
-    
+    printf("Qual é o código: \n");
+    scanf("%c", código);
 
+    printf("Qual é o Nome da cidade: \n");
+    scanf("%c", nomedaCidade);
 
+    printf("Quantidade da população: \n");
+    scanf("%d", população);
 
+    printf("Qual é a area km²: \n");
+    scanf("%e", areakm²);
 
+    printf("Qual o PIB: \n");
+    scanf("%e", PIB);
+
+    printf("Quantidade de número de pontos turísticos: \n");
+    scanf("%d", númerodepontosturisticos);
 
     return 0;
 }
