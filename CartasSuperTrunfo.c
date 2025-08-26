37275d190170e962fd5e87694878890a81642015
Tiago Dourado da Silva <tdourado412@gmail.com>
2025-08-26 16:48:49 +0000

    Desafio aventureiro carta 1 finalizada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,11 @@ int main(){
     char nomedaCidade[20] = "Manaus";
     int população = 2069689;
     float areakm² = 11402;
-    float PIB = 103;
+    float PIB = 103000000000;
     int númerodepontosturisticos = 35;
+    float densidadepopulacional = população/areakm²;
+    float PIBpercapita = PIB/população;
+
 
     printf("Carta 1: \n");
     printf("Estado: %c\n",estado);
@@ -16,8 +19,10 @@ int main(){
     printf("Nome da cidade: %s\n", nomedaCidade);
     printf("População: %d\n", população);
     printf("Área km²: %.fkm²\n", areakm²);
-    printf("PIB: %fbilhões\n", PIB);
+    printf("PIB: %.fbilhões\n", PIB);
     printf("Número de pontos turísticos: %d\n", númerodepontosturisticos);
+    printf("Densidade da população: %.2f\n", densidadepopulacional);
+    printf("PIB per capita: %.2f\n", PIBpercapita);
 
 
     return 0;
